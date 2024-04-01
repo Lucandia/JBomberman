7b6fe7927aa387dcbd8239d949fd738d8675a453
Lucandia <lmonari@bpe-mac-015w.fritz.box>
2024-04-01 02:07:03 +0200

    trying to make runnable jar on his own

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.lucandia {
-    requires javafx.controls;
-    requires javafx.fxml;
-    //requires transitive javafx.graphics;
-
-    opens com.lucandia to javafx.fxml;
-    exports com.lucandia;
-}
