b7fc2c2aaea39f9539f61d46384bb49661264e5a
Lucandia <lmonari@bpe-mac-015w.fritz.box>
2024-04-03 00:04:42 +0200

    added music

---
+++ src/main/java/com/lucandia/BackgroundMusic.java
@@ -0,0 +1,45 @@
+package com.lucandia;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import java.net.URL;
+
+public class BackgroundMusic {
+
+    private MediaPlayer mediaPlayer;
+
+    public void playMusic(String audioFileName) {
+        try {
+            // Loading the audio file
+            URL musicFileUrl = getClass().getResource("/audio/" + audioFileName);
+            if (musicFileUrl != null) {
+                Media media = new Media(musicFileUrl.toExternalForm());
+                MediaPlayer mediaPlayer = new MediaPlayer(media);
+                mediaPlayer.setOnEndOfMedia(new Runnable() {
+                    @Override
+                    public void run() {
+                        mediaPlayer.seek(javafx.util.Duration.ZERO); // Loop back to the start
+                    }
+                });
+                mediaPlayer.play();
+                mediaPlayer.setVolume(0.7);
+            } else {
+                System.err.println("Could not find the audio file: " + audioFileName);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("Error playing the audio file.");
+        }
+    }
+
+    public void stopMusic() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+        }
+    }
+
+    public void setVolume(double volume) {
+        if (mediaPlayer != null) {
+            mediaPlayer.setVolume(volume);
+        }
+    }
+}
